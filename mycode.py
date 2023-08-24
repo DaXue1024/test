16d1411a8a2344bbcc6e54c887843d09697edbb7
lzm7788 <wzm2256@qq.com>
2023-08-24 18:40:21 +0200

    code cleaning

--- mycode.py
+++ mycode.py
@@ -1,8 +1,6 @@
-import Bio
 import urllib
 import re
-import os 
-from Bio import Entrez, SeqIO
+from Bio import Entrez
 
 
 f = open('id.txt','r')
@@ -14,19 +12,33 @@ for line in f:
     gene_id=list1[1]
     url = 'https://www.ebi.ac.uk/ena/browser/api/embl/' + gene_id + '?lineLimit=1000'
     try:
+        print(f'Requesting {url}')
         source=urllib.request.urlopen(url).read()
+        print('Requesting done.')
+    except:
+        fw.write(gene_id+'\n')
+        print('Error Requesting url.')
+        continue
+
+    try:
         complement=re.findall('complement.*[^)]',source.decode('utf-8'))
         start = complement[0].strip().split(':')[1].split('.')[0]
-        print (type(start))
-        print (int(start)-4000)
         genome = complement[0].split(':')[0].split('(')[1]
         stop = complement[0].split(':')[1].split('..')[1].replace(')','')
+        print(f'Fetching Genome {genome} start {start} stop: {stop}')
         handle = Entrez.efetch(db="nuccore", id=genome,rettype="fasta", retmode="text", seq_start=(int(start)-4000), seq_stop=(int(stop)+4000))
-        print (handle.read())
-        parse_efetch_gb = SeqIO.parse(handle, "fasta")
+    except:
+        fw.write(gene_id+'\n')
+        print('Entrez fetch error.')
+        continue
+    
+    try:
         read_efetch_gb = handle.read()
+        print(read_efetch_gb)
         with open(gene_id +".fasta","w") as file:
             file.write(read_efetch_gb)
     except:
         fw.write(gene_id+'\n')
-       # print ('except')
+        print ('Write error.')
+
+fw.close()
